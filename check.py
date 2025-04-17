4448a788eb4d9c8b3ebd932937f7ad3c80d66efd
Teja <67798221+kangScripter@users.noreply.github.com>
2025-04-17 16:26:16 +0530

    Add files via upload

---
+++ check.py
@@ -0,0 +1,11 @@
+from words import abusive_words
+
+def detch_abuse(text):
+    m = []
+    for t in text.split(" "):
+       m.append('*' * len(t)) if t.lower() in abusive_words else m.append(t)
+    return " ".join(m)
+
+while True:
+    user_text = input("User: ")
+    print(detch_abuse(user_text))
